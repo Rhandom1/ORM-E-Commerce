b35aea7bc02981aeeca6af8357984be011379e21
Rhandom1 <73659834+Rhandom1@users.noreply.github.com>
2021-09-24 11:57:09 -0500

    Moved out of develop folder, tested Category success

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { UPSERT } = require('sequelize/types/lib/query-types');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
@@ -39,10 +38,9 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new category
   try {
-    const categoryData = await UPSERT.create({
-      id: req.body.id,
-      category_name: req.body.category_name,
-    });
+    const categoryData = await Category.create(
+      req.body
+    );
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
@@ -66,7 +64,7 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
     const categoryData = await Category.destroy({
