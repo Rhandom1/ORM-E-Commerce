caf4f21c333c412c02408a89371c5250701f9895
Rhandom1 <73659834+Rhandom1@users.noreply.github.com>
2021-09-27 22:30:16 -0500

    Fixed some routing errors, tag still not connecting

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const { Category, Product} = require('../../models');
 
 // The `/api/categories` endpoint
 
@@ -7,20 +7,21 @@ router.get('/', async (req, res) => {
   // find all categories
   try {
     const categoryData = await Category.findAll({
-      include: [{ model: Product }, { model: Tag }],
+      // be sure to include its associated Products
+      include: [{ model: Product }],
     });
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
-  // be sure to include its associated Products
+  
 });
 
 router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   try {
     const categoryData = await Category.findByPk(req.params.id, {
-      include: [{ model: Product }, { model: Tag }],
+      include: [{ model: Product }],
     });
 
     if(!categoryData) {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,10 +7,11 @@ router.get('/', async (req, res) => {
   // find all tags
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product }, { model: Tag }],
+      include: [{ model: Product }, { through: ProductTag }],
     });
     res.status(200).json(tagData);
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
   // be sure to include its associated Product data
